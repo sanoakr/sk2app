355ff45eeafae180412313b3cf537a958d5a09bf
SANO Akira <sano@math.ryukoku.ac.jp>
2021-03-03 18:24:35 +0900

    [Android] add explanation images, temporary commit

--- Android/sk2/app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivityUi.kt
+++ Android/sk2/app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivityUi.kt
@@ -2,8 +2,13 @@ package jp.ac.ryukoku.st.sk2
 
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.Typeface
 import android.text.InputType.*
+import android.text.Spannable
+import android.text.style.BulletSpan
+import android.text.style.LeadingMarginSpan
 import android.view.Gravity
+import androidx.core.view.marginBottom
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_ATMARK
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_PASSWD
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_UID
@@ -11,6 +16,7 @@ import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.COLOR_BACKGROUND
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.COLOR_MAIN
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_HUGE
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_LARGE
+import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_NORMAL
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_TINY
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.APP_NAME
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.APP_TITLE
@@ -87,50 +93,97 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
                 //backgroundColor = COLOR_NORMAL
                 onClick {
                     when {
+                        //　ユーザー名が空
                         user.text.toString().isBlank() ->
                             toast(TOAST_LOGIN_ATTEMPT_UID).setGravity(Gravity.CENTER, 0, 0)
+                        // テストユーザーではなく、パスワードが空
                         !user.text.toString().startsWith(NAME_START_TESTUSER)
                                 && passwd.text.toString().isBlank() ->
                             toast(TOAST_LOGIN_ATTEMPT_PASSWD).setGravity(
-                                Gravity.CENTER,
-                                0,
-                                0
+                                    Gravity.CENTER,
+                                    0,
+                                    0
                             )
+                        // ユーザー名がメールアドレス（@ が含まれている）
                         user.text.toString().contains('@') ->
                             toast(TOAST_LOGIN_ATTEMPT_ATMARK).setGravity(
-                                Gravity.CENTER,
-                                0,
-                                0
+                                    Gravity.CENTER,
+                                    0,
+                                    0
                             )
                         else ->
+                            // 1つめの説明
                             alert {
-                                positiveButton("同意します") { _ ->
-                                    //runBlocking(Dispatchers.IO) {
-                                        ui.owner.attemptLogin(
-                                            user.text.toString().trim(),
-                                            passwd.text.toString(),
-                                            SERVER_ORGNIZATION
-                                        )
-                                    //}
-                                }
-                                negativeButton("同意しません") { }
-
                                 customView {
                                     verticalLayout {
-                                        textView("Sk2 プライバシーポリシーの承認") {
-                                            textSize = TEXT_HUGE
-                                            textColor = Color.WHITE
-                                            backgroundColor = COLOR_NORMAL_LIGHT
-                                            padding = dip(8)
-                                        }.lparams {
-                                            width = matchParent
-                                        }
-                                        webView {
-                                            settings.javaScriptEnabled = true
-                                            //clearCache(true)
-                                            loadUrl(PRIVACY_URI)
-                                        }
+                                        padding = dip(32)
+                                        textView("sk2 は大学キャンパス内でのみ、\nバックグラウンドの位置情報を取得します") {
+                                            textSize = TEXT_LARGE
+                                            textColor = Color.BLUE
+                                            typeface = Typeface.DEFAULT_BOLD
+                                        }.lparams { weight = 1f }
+                                        textView("あいうえお") {
+                                            textSize = TEXT_LARGE
+                                            textColor = Color.BLACK
+                                        }.lparams { weight = 1f }
+                                        imageView(R.drawable.explain1) {
+
+                                        }.lparams { weight = 1f }
+                                        textView("かきくけこ") {
+                                            textSize = TEXT_NORMAL
+                                            textColor = Color.BLACK
+                                        }.lparams { weight = 1f }
                                     }
+                                }
+                                positiveButton("次へ") { _ ->
+
+                                    // 2つめの説明
+                                    alert {
+                                        customView {
+                                            padding = dip(8)
+                                            verticalLayout {
+                                                textView("sk2 は手動送信でのみ、\n緯度経度を含むおおよその位置情報を取得します") {
+                                                    textSize = TEXT_LARGE
+                                                    textColor = Color.BLUE
+                                                    typeface = Typeface.DEFAULT_BOLD
+                                                }
+                                                imageView(R.drawable.explain2)
+                                            }
+                                        }
+                                        positiveButton("次へ") { _ ->
+
+                                            // 最後にプライバシーポリシーを承認
+                                            alert {
+                                                customView {
+                                                    verticalLayout {
+                                                        textView("sk2 プライバシーポリシーの承認") {
+                                                            textSize = TEXT_HUGE
+                                                            textColor = Color.WHITE
+                                                            backgroundColor = COLOR_NORMAL_LIGHT
+                                                            padding = dip(8)
+                                                        }.lparams {
+                                                            width = matchParent
+                                                        }
+                                                        webView {
+                                                            settings.javaScriptEnabled = true
+                                                            //clearCache(true)
+                                                            loadUrl(PRIVACY_URI)
+                                                        }
+                                                    }
+                                                }
+                                                positiveButton("同意します") { _ ->
+                                                    //runBlocking(Dispatchers.IO) {
+                                                    ui.owner.attemptLogin(
+                                                            user.text.toString().trim(),
+                                                            passwd.text.toString(),
+                                                            SERVER_ORGNIZATION
+                                                    )
+                                                    //}
+                                                }
+                                                negativeButton("同意しません") { }
+                                            }.show()
+                                        }
+                                    }.show()
                                 }
                             }.show()
                     }
