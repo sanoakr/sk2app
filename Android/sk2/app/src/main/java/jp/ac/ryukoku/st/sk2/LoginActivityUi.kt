d2f10da5b2d2ce62bd3aa3afb31acc03bf2451a7
SANO Akira <sano@math.ryukoku.ac.jp>
2021-03-04 01:48:59 +0900

    [Android] add opening explanation sheets, release 2.0.1

--- Android/sk2/app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivityUi.kt
+++ Android/sk2/app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivityUi.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Typeface
 import android.text.InputType.*
-import android.text.Spannable
-import android.text.style.BulletSpan
-import android.text.style.LeadingMarginSpan
 import android.view.Gravity
-import androidx.core.view.marginBottom
+import android.view.View
+import android.view.ViewManager
+import android.widget.ImageView
+import android.widget.LinearLayout
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_ATMARK
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_PASSWD
 import jp.ac.ryukoku.st.sk2.LoginActivity.Companion.TOAST_LOGIN_ATTEMPT_UID
@@ -16,7 +16,6 @@ import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.COLOR_BACKGROUND
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.COLOR_MAIN
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_HUGE
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_LARGE
-import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_NORMAL
 import jp.ac.ryukoku.st.sk2.MainActivityUi.Companion.TEXT_TINY
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.APP_NAME
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.APP_TITLE
@@ -24,8 +23,6 @@ import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.COLOR_HINT
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.COLOR_NORMAL_LIGHT
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.NAME_START_TESTUSER
 import jp.ac.ryukoku.st.sk2.Sk2Globals.Companion.SERVER_ORGNIZATION
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
@@ -44,6 +41,7 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
         const val HINT_UID = "全学統合認証ID"
         const val HINT_PASSWD = "パスワード"
     }
+
     /** ////////////////////////////////////////////////////////////////////////////// **/
     @SuppressLint("SetJavaScriptEnabled")
     override fun createView(ui: AnkoContext<LoginActivity>) = with(ui) {
@@ -55,7 +53,7 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
                 id = TITLE
                 textSize = TEXT_HUGE
                 textColor = COLOR_MAIN
-            }.lparams{
+            }.lparams {
                 centerHorizontally(); alignParentTop()
                 topMargin = dip(100); bottomMargin = dip(50)
             }
@@ -64,7 +62,7 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
                 id = LABEL
                 textSize = TEXT_LARGE
                 textColor = Color.BLACK
-            }.lparams{
+            }.lparams {
                 below(TITLE); leftMargin = dip(4)
             }
             /** ////////////////////////////////////////////////////////////////////////////// **/
@@ -75,6 +73,7 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
                 hintTextColor = COLOR_HINT
                 inputType = TYPE_TEXT_VARIATION_EMAIL_ADDRESS
             }.lparams { below(LABEL); width = matchParent; height = dip(50) }
+
             /** ////////////////////////////////////////////////////////////////////////////// **/
             val passwd = editText {
                 id = PASS
@@ -117,45 +116,70 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
                                 customView {
                                     verticalLayout {
                                         padding = dip(32)
-                                        textView("sk2 は大学キャンパス内でのみ、\nバックグラウンドの位置情報を取得します") {
-                                            textSize = TEXT_LARGE
+                                        lparams(width = matchParent, height = matchParent)
+
+                                        textView("sk2 は教室位置情報をバックグラウンドで取得します") {
+                                            textSize = TEXT_HUGE
                                             textColor = Color.BLUE
                                             typeface = Typeface.DEFAULT_BOLD
-                                        }.lparams { weight = 1f }
-                                        textView("あいうえお") {
-                                            textSize = TEXT_LARGE
-                                            textColor = Color.BLACK
-                                        }.lparams { weight = 1f }
-                                        imageView(R.drawable.explain1) {
+                                        }.lparams {
+                                            width = wrapContent
+                                            height = wrapContent
+                                            margin = dip(32)
+                                        }
 
-                                        }.lparams { weight = 1f }
-                                        textView("かきくけこ") {
-                                            textSize = TEXT_NORMAL
-                                            textColor = Color.BLACK
-                                        }.lparams { weight = 1f }
+                                        imageView(R.drawable.explain1) {
+                                            scaleType = ImageView.ScaleType.CENTER_INSIDE
+                                        }.lparams {
+                                            width = matchParent
+                                            height = dip(150)
+                                            padding = dip(0)
+                                            margin = dip(16)
+                                        }
+                                        listedTextView("sk2アプリを一度起動しておけば、自動で出席記録が行われます。")
+                                        listedTextView("教室移動などでビーコン信号が変化すると自動で出席情報が送信されます。")
+                                        listedTextView("自動送信は、龍谷大学瀬田キャンパス内で、授業実施時間にのみ実行されます。")
+                                        listedTextView("自動送信の記録頻度は最短でも10分に1回です。")
                                     }
                                 }
                                 positiveButton("次へ") { _ ->
-
                                     // 2つめの説明
                                     alert {
                                         customView {
-                                            padding = dip(8)
                                             verticalLayout {
-                                                textView("sk2 は手動送信でのみ、\n緯度経度を含むおおよその位置情報を取得します") {
-                                                    textSize = TEXT_LARGE
+                                                padding = dip(32)
+                                                lparams(width = matchParent, height = matchParent)
+
+                                                textView("sk2 は手動送信でのみ、緯度経度を含むおおよその位置情報を取得します") {
+                                                    textSize = TEXT_HUGE
                                                     textColor = Color.BLUE
                                                     typeface = Typeface.DEFAULT_BOLD
+                                                }.lparams {
+                                                    width = wrapContent
+                                                    height = wrapContent
+                                                    margin = dip(32)
                                                 }
-                                                imageView(R.drawable.explain2)
+
+                                                imageView(R.drawable.explain2) {
+                                                    scaleType = ImageView.ScaleType.CENTER_INSIDE
+                                                }.lparams {
+                                                    width = matchParent
+                                                    height = dip(150)
+                                                    padding = dip(0)
+                                                    margin = dip(16)
+                                                }
+
+                                                listedTextView("手動送信は「出席」ボタンで表示される「送信場所」を選択すると実行されます。")
+                                                listedTextView("手動記録では、GPSや基地局情報などからおおよその緯度経度情報が送信されます。")
+                                                listedTextView("手動記録は、オンライン授業での出席記録や災害時の安否確認などに利用されます。")
                                             }
                                         }
                                         positiveButton("次へ") { _ ->
-
                                             // 最後にプライバシーポリシーを承認
                                             alert {
                                                 customView {
                                                     verticalLayout {
+                                                        lparams(width = matchParent, height = wrapContent)
                                                         textView("sk2 プライバシーポリシーの承認") {
                                                             textSize = TEXT_HUGE
                                                             textColor = Color.WHITE
@@ -200,6 +224,26 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
             }.lparams {
                 alignParentBottom(); alignParentEnd()
             }
+        }
+    }
+
+    /** ////////////////////////////////////////////////////////////////////////////// **/
+    fun ViewManager.listedTextView(text: String) = linearLayout {
+        padding = dip(16)
+        lparams(width = matchParent, height = wrapContent)
+        textView("● ") {
+            textSize = TEXT_LARGE
+            textColor = Color.BLUE
+        }.lparams {
+            width = wrapContent
+            height = wrapContent
+        }
+        textView(text) {
+            textSize = TEXT_LARGE
+            textColor = Color.BLACK
+        }.lparams {
+            width = wrapContent
+            height = wrapContent
         }
     }
 }
